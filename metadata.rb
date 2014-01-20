16fe2fae3a40bb3cb680c12c14d5dfffbe035bad
Conrad Kramer <conrad@kramerapps.com>
2014-01-20 17:46:52 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "conrad@kramerapps.com"
 license          "Apache 2.0"
 description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+version          "2.0.1"
 
 depends          "nodejs"
 depends          "application"
