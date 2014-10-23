fa9894bb80b06dc4429604cd5a035637d1af4b8a
Jake Plimack <jake.plimack@gmail.com>
2014-10-23 16:25:39 -0600

    Berkify and lock down nodejs cookbook version

--- providers/nodejs.rb
+++ providers/nodejs.rb
@@ -22,7 +22,7 @@ include Chef::DSL::IncludeRecipe
 
 action :before_compile do
 
-  include_recipe 'nodejs::install_from_source'
+  include_recipe 'nodejs::nodejs_from_source'
 
   if new_resource.npm
     include_recipe 'nodejs::npm'
