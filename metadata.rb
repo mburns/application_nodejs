7366ad4543dad17e4b29da240ca7aa4eb7d31bfd
Jake Plimack <jake.plimack@gmail.com>
2014-11-05 16:55:03 -0700

    provider to setup nginx/passenger

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,11 @@ description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.1"
 
-depends          "nodejs"
-depends          "application"
+depends 'nodejs'
+depends 'application'
 depends 'runit'
 depends 'apache2'
+depends 'nginx'
 depends 'passenger_apache2'
 supports 'ubuntu', ">= 12.04"
+supports 'debian'
