437e804deae5f8bdfabd07e39a68a5ae05ab2876
Michael Burns <michael.burns@rackspace.com>
2015-06-28 12:44:54 -0700

    add source and issues URLs

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'Apache 2.0'
 description 'Deploys and configures Node.js applications'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.2'
+source_url 'https://github.com/mburns/application_nodejs' if respond_to?(:source_url)
+issues_url 'https://github.com/mburns/application_nodejs/issues' if respond_to?(:issues_url)
 
 depends 'nodejs'
 depends 'application'
