787170fdbb906df8cadd52d8f06e675ded8f624b
Michael Burns <michael.burns@rackspace.com>
2015-07-01 10:41:38 -0700

    declare default and available actions

--- resources/nodejs.rb
+++ resources/nodejs.rb
@@ -20,6 +20,10 @@
 
 include ApplicationCookbook::ResourceBase
 
+actions :before_compile, :before_deploy, :before_migrate, :before_symlink, :before_restart, :after_restart
+
+default_action :before_compile
+
 attribute :npm, kind_of: [NilClass, TrueClass, FalseClass], default: true
 attribute :template, kind_of: [String, NilClass], default: nil
 attribute :entry_point, kind_of: String, default: 'app.js'
